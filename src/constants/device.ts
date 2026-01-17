98cbf4bfed340672edec8660e7104e195b44b2ab
Mihai Stanciu <mihai.stanciu@gmail.com>
2026-01-17 20:19:36 +0200

    feat: add Q Revo MaxV support and refactor clean mode logic
    Adds support for the Roborock Q Revo MaxV (QREVO_MAXV) and simplifies getSupportedCleanModes by validating against the SMART_MODELS set, removing the need for hardcoded switch cases.

--- src/constants/device.ts
+++ src/constants/device.ts
@@ -9,7 +9,7 @@ import { DeviceModel } from '../roborockCommunication/Zmodel/deviceModel.js';
  * Set of device models that support smart planning features.
  * These models have advanced clean mode capabilities and extended feature sets.
  */
-export const SMART_MODELS = new Set<string>([DeviceModel.QREVO_EDGE_5V1, DeviceModel.QREVO_PLUS]);
+export const SMART_MODELS = new Set<string>([DeviceModel.QREVO_EDGE_5V1, DeviceModel.QREVO_PLUS, DeviceModel.QREVO_MAXV]);
 
 /**
  * Default delay for unregistering devices on shutdown (milliseconds).
